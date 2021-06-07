a6eef379399e9b93d9cc29b833dd9907a0e4661d
lxl <dreammakerlxl@gmail.com>
2021-06-07 19:37:29 +0800

    init

---
+++ agents/__init__.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+
+__all__ = ['download']
+
+
+def _python2_env():
+    """function to check python version for compatibility handling"""
+    if sys.version_info[0] < 3:
+        return True
+    else:
+        return False
+
+
+def _python3_env():
+    """function to check python version for compatibility handling"""
+    return not _python2_env()
+
+
+def download(download_url=None, filename_to_save=None):
+    if download_url is None or download_url == '':
+        print('[Agent][ERROR] - download URL missing for download()')
+        return False
+
+    if filename_to_save is None or filename_to_save == '':
+        download_url_tokens = download_url.split('/')
+
+    # if not given, use last part of url as filename to save
+    if filename_to_save is None or filename_to_save == '':
+        download_url_tokens = download_url.split('/')
+        filename_to_save = download_url_tokens[-1]
+
+    # delete existing file if exist to ensure freshness
+    if os.path.isfile(filename_to_save):
+        os.remove(filename_to_save)
+
+    # handle case where url is invalid or has no content
+    try:
+        if _python2_env():
+            import urllib
+            urllib.urlretrieve(download_url, filename_to_save)
+        else:
+            import urllib.request
+            urllib.request.urlretrieve(download_url, filename_to_save)
+
+    except Exception as e:
+        print('[Agent][ERROR] - failed downloading from ' + download_url + '...')
+        print(str(e))
+        return False
+
+    # take the existence of downloaded file as success
+    if os.path.isfile(filename_to_save):
+        return True
+
+    else:
+        print('[Agent][ERROR] - failed downloading to ' + filename_to_save)
+        return False
